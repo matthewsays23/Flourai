99cf7836cdb54a5dac9b0584411c78524f531dee
Jason Jones <jasonjones@Jasons-MacBook-Air-2.local>
2026-04-10 12:50:53 -0400

    Test

--- src/commands/Community/roles.js
+++ src/commands/Community/roles.js
@@ -13,17 +13,15 @@ module.exports = {
 
   async execute(interaction) {
     const embed = new EmbedBuilder()
+    .setTitle("<:emoji_41:1113830951877886084>  Want to stay up-to date on Flourai's operations?")
       .setColor("#2f3136")
       .setImage("https://cdn.discordapp.com/attachments/1330406002688000085/1337683303230214184/8.png?ex=69d9f528&is=69d8a3a8&hm=c9f65645a072fd24374d18d419e443fc0603689656383ea8f1f25afe9505adf6&")
       .setDescription(
         [
-          "## <:emoji_41:1113830951877886084>  Want to stay up-to date on Flourai's operations?",
           "",
           "Flourai now offers self-assignable role management, allowing you to customize which topics you receive notifications for. To receive pings for a specific topic, please use the buttons below and select the appropriate role. If you wish to remove a role, simply return to the menu and deselect it.",
-          "",
-          "---",
-          "",
-          "If you have any questions or require assistance, please contact a member of the **Leadership Team**."
+          "-----",
+          "-# If you have any questions or require assistance, please contact a member of the **Leadership Team**."
         ].join("\n")
       );
 
@@ -31,7 +29,7 @@ module.exports = {
       new ButtonBuilder()
         .setCustomId("notification_roles")
         .setLabel("Notification Roles")
-        .setStyle(ButtonStyle.Primary),
+        .setStyle(ButtonStyle.Secondary),
 
     );
 
