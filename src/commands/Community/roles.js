3574f9c1f44a34918120fcba219f3252dfbcd820
Jason Jones <jasonjones@Jasons-MacBook-Air-2.local>
2026-04-10 12:46:53 -0400

    Test

--- src/commands/Community/roles.js
+++ src/commands/Community/roles.js
@@ -3,51 +3,41 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  EmbedBuilder,
 } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("addbuttons")
-    .setDescription("Add buttons to an existing message")
-    .addStringOption(option =>
-      option.setName("messageid")
-        .setDescription("Message ID")
-        .setRequired(true)
-    ),
+    .setName("rolespanel")
+    .setDescription("Send the Flourai roles panel"),
 
   async execute(interaction) {
-    const messageId = interaction.options.getString("messageid");
-
-    const channel = interaction.channel;
-
-    try {
-      const message = await channel.messages.fetch(messageId);
-
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          .setCustomId("notification_roles")
-          .setLabel("Notification Roles")
-          .setStyle(ButtonStyle.Secondary),
+    const embed = new EmbedBuilder()
+      .setColor("#2f3136")
+      .setImage("https://cdn.discordapp.com/attachments/1330406002688000085/1337683303230214184/8.png?ex=69d9f528&is=69d8a3a8&hm=c9f65645a072fd24374d18d419e443fc0603689656383ea8f1f25afe9505adf6&")
+      .setDescription(
+        [
+          "## <:emoji_41:1113830951877886084>  Want to stay up-to date on Flourai's operations?",
+          "",
+          "Flourai now offers self-assignable role management, allowing you to customize which topics you receive notifications for. To receive pings for a specific topic, please use the buttons below and select the appropriate role. If you wish to remove a role, simply return to the menu and deselect it.",
+          "",
+          "---",
+          "",
+          "If you have any questions or require assistance, please contact a member of the **Leadership Team**."
+        ].join("\n")
       );
 
-      await message.edit({
-  content: null,
-  embeds: [],
-  components: [row],
-});
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("notification_roles")
+        .setLabel("Notification Roles")
+        .setStyle(ButtonStyle.Primary),
 
-      await interaction.reply({
-        content: "✅ Buttons added.",
-        ephemeral: true,
-      });
+    );
 
-    } catch (err) {
-      console.error(err);
-
-      await interaction.reply({
-        content: "❌ Failed to edit message.",
-        ephemeral: true,
-      });
-    }
+    await interaction.reply({
+      embeds: [embed],
+      components: [row],
+    });
   },
 };
