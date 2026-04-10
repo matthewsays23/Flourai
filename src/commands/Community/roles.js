17a1604d51ab3c615325b6671f01271192a0aa45
Jason Jones <jasonjones@Jasons-MacBook-Air-2.local>
2026-04-10 13:49:58 -0400

    Fixed

--- src/commands/Community/roles.js
+++ src/commands/Community/roles.js
@@ -9,11 +9,12 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("rolespanel")
-    .setDescription("Send the Flourai roles panel"),
+    .setDescription("Send the Flourai roles panel")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
   async execute(interaction) {
     const embed = new EmbedBuilder()
-      .setTitle("<:emoji_41:1113830951877886084> Want to stay up-to date on Flourai's operations?")
+      .setTitle("<:emoji_41:1113830951877886084>  Want to stay up-to date on Flourai's operations?")
       .setColor("#2f3136")
       .setImage("https://cdn.discordapp.com/attachments/1330406002688000085/1337683303230214184/8.png?ex=69d9f528&is=69d8a3a8&hm=c9f65645a072fd24374d18d419e443fc0603689656383ea8f1f25afe9505adf6&")
       .setDescription(
