94f465def5af8fc37ee0139d28a179d887f862c4
Jason Jones <jasonjones@Jasons-MacBook-Air-2.local>
2026-04-13 10:47:06 -0400

    Test

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const {
   REST,
   Routes,
   EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
 } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
