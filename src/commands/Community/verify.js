cba2ffd6675b6d6907509a1266ea41b690381b4a
Jason Jones <jasonjones@Jasons-MacBook-Air-2.local>
2026-04-20 20:06:10 -0400

    Test

--- src/commands/Community/verify.js
+++ src/commands/Community/verify.js
@@ -18,13 +18,11 @@ module.exports = {
 
     const embed = new EmbedBuilder()
       .setTitle("<:f_initial:1493040287143034993>  Flourai Verification")
-      .setColor("#294d38")
+      .setColor("#2f3136")
       .setImage('https://cdn.discordapp.com/attachments/1401654108561346720/1486355088489578556/welcomebanner.png?ex=69e77a8c&is=69e6290c&hm=5554b817cc10eaa0fc6f891c02a49c08f792aa7dccd2d49a881d12f668aac4be')
       .setDescription(
         [
-          "Connect your Roblox account to Discord to receive your verified server identity.",
-          "",
-          "Verification can update your nickname and assign roles based on your Flourai Roblox group rank.",
+          "Connect your Roblox account to Discord to receive your verified server identity. Verification can update your nickname and assign roles based on your Flourai Roblox group rank.",
           "",
           "-# If your roles do not update after verifying, please contact a member of the Leadership Team.",
         ].join("\n")
